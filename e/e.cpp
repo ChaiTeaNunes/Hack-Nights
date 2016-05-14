9c0863c30b7bb81481648f0baf93f444e53266c7
Chai Nunes <chaiteanunes@gmail.com>
2016-05-13 22:26:11 -0700

    Update e.cpp

--- e/e.cpp
+++ e/e.cpp
@@ -3,8 +3,8 @@
 using namesapce std;
 
 #define EEEEEEEE cout << "EEEEEEEE" << endl;
-#define E cout << "E" << endl;
 #define EEEEEE cout << "EEEEEE" << endl;
+#define E cout << "E" << endl;
 
 int main(int argc, char*argv) {
     EEEEEEEE
