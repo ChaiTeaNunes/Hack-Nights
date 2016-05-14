9857cae4c3372dcfcfd045c9bc551504f6d9690d
Chai Nunes <chaiteanunes@gmail.com>
2016-05-13 22:11:34 -0700

    Create e.cpp

---
+++ e/e.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namesapce std;
+
+#define EEEEEEEE cout << "EEEEEEEE" << endl;
+#define E cout << "E" << endl;
+#define EEEEEE cout << "EEEEEE" << endl;
+
+int main(int argc, char*argv) {
+    EEEEEEEE
+    E
+    EEEEEE
+    E
+    EEEEEEEE
+    
+    return 0;
+}
